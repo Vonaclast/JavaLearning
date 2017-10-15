f7574718d9f1b1de3afaadeb9ff288179bdcbbe0
Oskar Enmalm <oskarenmalm@gmail.com>
2017-10-15 21:26:12 +0200

    Hello, World!
    Trying to work out how to work on the desktop on the master branch. If it gets messy then I'll update how it's laid out to fit you guys.

---
+++ OskarEn/src/OskarEn001_01.java
@@ -0,0 +1,11 @@
+public class OskarEn001_01 {
+
+    public static void main(String[] arg){
+
+        String a = "Hello";
+        String b = "World!";
+
+        System.out.println(a + ", " + b + "!");
+    }
+
+}
