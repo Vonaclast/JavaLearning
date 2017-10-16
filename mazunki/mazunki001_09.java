6388c829155f1d4d03dd06f6d5ffb21706f4ab30
Rolf Vidar Hoksaas <32819373+mazunki@users.noreply.github.com>
2017-10-16 18:49:54 +0200

    Add files via upload

---
+++ mazunki/mazunki001_09.java
@@ -0,0 +1,10 @@
+class mazunki001_09 {
+	
+	public static void main(String[] args){
+
+		for (int i=0; i<100; i++){
+			System.out.println("This is line number " + (i+1) + ".");
+		}
+		return;
+	}		
+}
