d0ff9b9b6ed9ea311fbe1ccbae075c5992b21419
Oskar Enmalm <oskarenmalm@gmail.com>
2017-10-16 18:56:37 +0200

    Create intro array task

---
+++ OskarEn/src/OskarEn001_003.java
@@ -0,0 +1,13 @@
+public class OskarEn001_003 {
+
+    public static void main(String[] args){
+        String[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        System.out.print("Today is " + daysOfWeek[4]);
+        System.out.print(". ");
+        System.out.print("Tomorrow is " + daysOfWeek[5]);
+
+    }
+
+
+}
