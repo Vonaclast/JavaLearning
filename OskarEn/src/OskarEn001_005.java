2cdf9ea10e4274678f95a44c0a2a5e1094823378
Oskar Enmalm <oskarenmalm@gmail.com>
2017-10-16 19:07:44 +0200

    Create method with return value

---
+++ OskarEn/src/OskarEn001_005.java
@@ -0,0 +1,18 @@
+public class OskarEn001_005 {
+
+    public static void main(String[] args){
+
+        int first = 6;
+        int second = 9;
+
+        System.out.println(multiplyNumbers(first,second));
+
+    }
+
+
+    //Method that is called by the main method that prints the product of two numbers
+    //returns an integer which is printed by the main method
+    public static int multiplyNumbers(int a, int b){
+        return (a * b);
+    }
+}
