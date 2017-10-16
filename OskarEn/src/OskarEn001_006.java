e5ae4636b3d4c4e274041c8ed57ae76a332f61d3
Oskar Enmalm <oskarenmalm@gmail.com>
2017-10-16 19:19:17 +0200

    Create sum and evaluation with if else loop

---
+++ OskarEn/src/OskarEn001_006.java
@@ -0,0 +1,27 @@
+public class OskarEn001_006 {
+
+    public static void main(String[] args) {
+
+        int a = 21;
+        int b = 15;
+        int c = returnSum(a,b);
+        print40(c);
+
+    }
+
+    public static int returnSum(int a, int b){
+        return a + b;
+    }
+
+    //Example only wanted first to statements, this would however print the line falsely when the sum is <=30
+
+    public static void print40(int c){
+        if(c>40){System.out.println(c + " is bigger than 40.");}
+
+        else {
+            if(c>30){System.out.println(c + " is bigger than 30 and smaller than 40");}
+
+            else{System.out.println(c + " is smaller than or equal to 30");}
+        }
+    }
+}
