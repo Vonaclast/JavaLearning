c5a2e28e6d8d8f82da5a0272dd2dba0c74d3cf9e
Rolf Vidar Hoksaas <32819373+mazunki@users.noreply.github.com>
2017-10-16 18:04:21 +0200

    Add files via upload

---
+++ mazunki/mazunki001_05.java
@@ -0,0 +1,15 @@
+class mazunki001_05 {
+
+	static int a = 6, b=9;
+
+	public static int multiplyIntegers(int x, int y){
+		return (x*y);
+	}
+
+	public static void main(String[] args){
+
+		System.out.println(multiplyIntegers(a,b));
+
+		return;
+	}
+}
