080a78fb411d7573101cf7eea96cab341f09980d
Rolf Vidar Hoksaas <32819373+mazunki@users.noreply.github.com>
2017-10-16 18:17:39 +0200

    Add files via upload

---
+++ mazunki/mazunki001_07.java
@@ -0,0 +1,14 @@
+class mazunki001_07 {
+	
+	public static void main(String[] args){
+
+		int i=0;
+
+		while(i<100){
+			System.out.println("This is sentce number #" + (i+1));
+			i++;
+		}
+
+		return;
+	}
+}
