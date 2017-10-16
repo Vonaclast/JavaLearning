49599bc4124019777ff4aa62d37b81b92fad0693
Oskar Enmalm <oskarenmalm@gmail.com>
2017-10-16 19:02:24 +0200

    addNumbers method

---
+++ OskarEn/src/OskarEn001_004.java
@@ -0,0 +1,17 @@
+public class OskarEn001_004 {
+
+    public static void main(String[] args){
+
+        int first = 25;
+        int second = 70;
+
+        addNumbers(first,second);
+
+    }
+
+
+    //Method that is called by the main method that prints the sum of two numbers
+    public static void addNumbers(int a, int b){
+        System.out.println(a + b);
+    }
+}
