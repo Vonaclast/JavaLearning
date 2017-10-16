cfbcd7b3ed7bd710a4b367d52a1f487de6c1c26a
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2017-10-16 15:21:31 +0200

    Creation of 001_02: Math Basics

---
+++ Hayaan/001_01
@@ -0,0 +1,8 @@
+public class Github {
+    static void main(String[] args) {
+        String a = "Hello";
+        String b = "World";
+
+        System.out.println(a + " " + b + "!");
+    }
+}
---
+++ Hayaan/001_02.java
@@ -0,0 +1,12 @@
+public class Github {
+    public static void main(String[] args) {
+        int num1 = 100;
+        int num2 = 20;
+
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+        System.out.println(num1/num2);
+        System.out.println(num1%num2);        
+    }
+}
