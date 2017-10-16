9fd57987eef225285d7e287d652125347c8c8627
Rolf Vidar Hoksaas <32819373+mazunki@users.noreply.github.com>
2017-10-16 18:11:08 +0200

    Add files via upload

---
+++ mazunki/mazunki001_06.java
@@ -0,0 +1,16 @@
+class mazunki001_06 {
+	
+	public static void main(String[] args){
+		int x = 21, y=15;
+		int z = x+y;
+
+		if(z>40){
+		System.out.println("It is bigger than 40. ");
+		}
+		else if ((z>30) && (z<40)){
+			System.out.println("It is between 30 and 40.");
+		}
+
+		return;
+	}
+}
