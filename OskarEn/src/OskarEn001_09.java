f6b7e60dcf1b5a5bc9995c26db5df8c9f2046521
Oskar Enmalm <oskarenmalm@gmail.com>
2017-10-17 23:37:23 +0200

    Create 001_009
    Just the sentence loop but with for instead of while.

---
+++ OskarEn/src/OskarEn001_09.java
@@ -0,0 +1,11 @@
+public class OskarEn001_09 {
+
+    public static void main(String[] args){
+
+        int x = 100; //This is how many times we want to print the sentence
+
+        for(int i=0; i<x; i++){
+            System.out.println("This is line number " + (i + 1));
+        }
+    }
+}
