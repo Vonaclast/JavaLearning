3c5e97f16ad28e945a500432a2431d3b11aaf8f2
Oskar Enmalm <oskarenmalm@gmail.com>
2017-10-18 02:12:15 +0200

    Create 03
    Print statements with properties of an object.

---
+++ OskarEn/002/OskarEn03.java
@@ -0,0 +1,12 @@
+public class OskarEn03 {
+
+public static void main(String[] args){
+    Car toyota = new Car(180, "Red",false);
+
+    System.out.print("This car has a maximum speed of " + toyota.maxSpeed + ", " );
+    System.out.print("is " + toyota.colour + " ");
+    System.out.print("The statement \"the engine is on\" is " + toyota.isEngineOn); 
+}
+
+
+}
